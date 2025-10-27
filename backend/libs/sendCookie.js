a7ef5f3d4eea4f3907dddf14000eed943655588c
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-10-27 19:45:12 +0530

    cookies settings changed

--- backend/libs/sendCookie.js
+++ backend/libs/sendCookie.js
@@ -2,7 +2,7 @@ export const sendCookie = (token, res) => {
     res.cookie('authToken', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 }
